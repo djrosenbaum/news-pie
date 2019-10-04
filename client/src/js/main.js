5e348484b4967a8600110b71c097dc3f20151021
DJ Rosenbaum <djrosenbaum@gmail.com>
2019-10-03 21:15:00 -0400

    provider only needed on the newsroom

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -2,6 +2,8 @@ import setupWeb3 from './setupWeb3';
 import setupState from './setupState';
 import setupDOM from './setupDOM';
 
+const urlParams = new URLSearchParams(window.location.search);
+
 async function init() {
   window.dapp = window.dapp || {};
   await setupWeb3();
@@ -10,5 +12,7 @@ async function init() {
 }
 
 window.addEventListener('load', () => {
-  init();
+  if (urlParams.has('place')) {
+    init();
+  }
 });
--- client/src/js/setupWeb3.js
+++ client/src/js/setupWeb3.js
@@ -1,14 +1,19 @@
 import { ethers } from 'ethers';
 
+const urlParams = new URLSearchParams(window.location.search);
+
 export default async function setupWeb3() {
-  if (window.ethereum || window.web3) {
-    if (window.ethereum) {
-      console.log('enable ethereum');
-      await window.ethereum.enable();
+  if (urlParams.get('place') === 'newsroom') {
+    if (window.ethereum || window.web3) {
+      if (window.ethereum) {
+        console.log('enable ethereum');
+        await window.ethereum.enable();
+      }
+      window.dapp.provider = new ethers.providers.Web3Provider(web3.currentProvider);
+    } else {
+      window.dapp.provider = new ethers.providers.BaseProvider();
     }
-    window.dapp.provider = new ethers.providers.Web3Provider(web3.currentProvider);
   } else {
-    console.log('no provider detected');
     window.dapp.provider = new ethers.providers.BaseProvider();
   }
 }
